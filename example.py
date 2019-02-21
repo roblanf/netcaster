f4d61b16dc83491d26d5113133a788e689bf4c8b
roblanf <rob.lanfear@gmail.com>
2019-02-22 10:14:44 +1100

    more examples
    including lenet-5

--- example.py
+++ example.py
@@ -4,6 +4,8 @@ from keras.layers import Dense
 import pandas as pd
 from lineage import Lineage
 import numpy as np
+from individual import Individual
+import pickle
 
 
 # load the data
@@ -33,44 +35,129 @@ X_train = (X_train - mean_px)/(std_px)
 Y_train = to_categorical(Y_train)
 
 
-# we split off a validation set for assessing fitness
+# Here's where we start doing things for the evolutionary algorithm
+
+# First we split off a validation set for assessing fitness
+# we'll use the validation set to calculate network fitness
+
 X_val = X_train[1:10000]
 Y_val = Y_train[1:10000]
 
 X_train = X_train[10001:]
 Y_train = Y_train[10001:]
 
+
 # We need a few things to establish a population
+
+# what will the output layer look like?
 output_config = Dense(units = 10, activation = 'softmax').get_config()
+
+# shape of the input data
 input_shape = (32, 32, 1)
+
+# name of the loss function to use
 loss = 'categorical_crossentropy'
 
 
+
+# Now we're ready to set up a population and let it evolve. Here are some examples
+
+# 1. A simple way of doing standard evolution 
 lineage = Lineage(input_shape, output_config, loss, X_train, Y_train, X_val, Y_val, trainsize = 31999, valsize = 5000)
-lineage.initialise(20)
-lineage.evolve([20]*50, num_parents = 2, selection = "weighted")
-lineage.evolve([50]*50, num_parents = 2, selection = "weighted")
+lineage.initialise(20) # start with 20 random genotypes
+lineage.evolve([20]*50) # 50 generations of evolution with 20 individuals in each
+
+
+# 2. A simple hill climbing algorithm
+hillclimb = Lineage(input_shape, output_config, loss, X_train, Y_train, X_val, Y_val, trainsize = 31999, valsize = 5000)
+hillclimb.initialise(5)
+# these settings keep the best individual around, and just breed one offspring from it in each generation
+hillclimb.evolve([2]*100, num_parents = 1, kill = 1, keep=1) 
 
 
 
-# establish a lineage with training and validation data
-lineage = Lineage(input_shape, output_config, loss, X_train, Y_train, X_val, Y_val, trainsize = 20000, valsize = 2000)
+# 3. Let's see if we can use evolution to improve on the classic lenet-5 network
 
-# start with a population of 10 random networks
-lineage.initialise(5)
+# Let's specify the lenet-5 network first...
+conv1 =    {"type": "conv",
+            "filters": 6,
+            "kernel": 5,
+            "strides": 1,
+            "padding": "valid",
+            "mutrate": 0.1,
+            "dropout": 0.0,
+            "norm": 0}
 
-# now we'll evolve it with 2 parents:
-	# 5 generations of population size 10
-	# 5 generations of population size 20
-lineage.evolve([10]*5 + [20]*5, num_parents = 2)
+pool1 =    {"type": "pool",
+			"pool_size": 2,
+			"strides": 2,
+			"padding": "valid",
+			"mutrate": 0.1}
 
-# then for another 50 generations of 40 individuals, with 4 parents each
-# we'll also kill off the least-fit 20 individuals in each generation
-# first we have to increase the population size to 40, to give us enough to kill 20
-lineage.evolve([40]*1, num_parents = 2)
+conv2 =    {"type": "conv",
+            "filters": 16,
+            "kernel": 5,
+            "strides": 1,
+            "padding": "valid",
+            "mutrate": 0.1,
+            "dropout": 0.0,
+            "norm": 0}
 
-# now we can do our next 50 generations
-lineage.evolve([40]*50, num_parents = 2, kill = 20)
+pool2 =    {"type": "pool",
+			"pool_size": 2,
+			"strides": 2,
+			"padding": "valid",
+			"mutrate": 0.1}
+
+full1 =    {"type": "full",
+		    "units": 120,
+		    "mutrate": 0.1,
+		    "dropout": 0.0,
+		    "norm": 0
+            }
+
+full2 =    {"type": "full",
+		    "units": 84,
+		    "mutrate": 0.1,
+		    "dropout": 0.0,
+		    "norm": 0
+            }
+
+network = [conv1, pool1, conv2, pool2, full1, full2]
+
+# train for 10 epochs with a batch size of 128
+training = [128]*10
+
+# hyperparameters of the model and the evolutioary algorithm
+params =   {"mutation": 0.1,
+            "recomb": 1.0, 
+            "optimiser": "Adam",
+            "learning_rate": 0.001,
+            "learning_rate_mutrate": 0.1, # mutation size for learning rate
+            "slip": 0.1, # chance of adding new layers to network
+            "epoch_mutrate": 0.1, # mutation rate applied to # epochs
+            "batchsize_mutrate": 0.1 # mutation rate for batch size
+            }
+
+lenet5 = {'params': params, 'network': network, 'training': training}
+
+# now let's write this out as a population
+# a population is a list of tuples: (fitness, training time, genotype)
+pop = [(0.0, 0.0, lenet5)] 
+with open('lenet5.pkl', 'wb') as myfile:
+    pickle.dump(pop, myfile)
+
+# now we'll make a lineage that starts with lenet5
+lenetx = Lineage(input_shape, output_config, loss, X_train, Y_train, X_val, Y_val, trainsize = 31999, valsize = 5000)
+# the recalculate fitness flag means that we'll start by evaluating the lenet5 genotype
+lenetx.initialise('lenet5.pkl', recalculate_fitness = True)
+
+# now we can evolve it how we like, here are two options
+# this is hill climbing
+lenetx.evolve([2], num_parents = 1, keep=1) # expand the population size to two 
+lenetx.evolve([2]*1000, num_parents = 1, kill = 1, keep=1) 
 
 
-
+# and this is standard evolution, but we'll keep the best network around in each generation
+lenetx.evolve([50], num_parents = 1, keep=1) # expand the population size to 50
+lenetx.evolve([50*20], keep=1) # 20 generations of 50 individuals, always keeping the fittest 
