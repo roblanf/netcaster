eb4e1fc717c3fef2d427a87af798fa287d8a38b9
roblanf <rob.lanfear@gmail.com>
2019-02-27 09:08:06 +1100

    change test code

--- example.py
+++ example.py
@@ -60,9 +60,9 @@ loss = 'categorical_crossentropy'
 
 
 # test mcmc code, delete
-mcmc = Lineage(input_shape, output_config, loss, X_train, Y_train, X_val, Y_val, trainsize = 3000, valsize = 900)
-mcmc.initialise(2)
-mcmc.hillclimb(2)
+test = Lineage(input_shape, output_config, loss, X_train, Y_train, X_val, Y_val, trainsize = 3000, valsize = 900)
+test.initialise(2)
+test.evolve([2]*2)
 
 
 
